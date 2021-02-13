af7450867a0bbb1c1382930d3a0422e41958183b
HackerIsho <hackerisho@localhost.localdomain>
2021-02-13 16:17:01 +0100

    bike rental file

---
+++ bikeRental.py
@@ -0,0 +1,142 @@
+import datetime
+
+class BikeRental:
+    def __init__(self, stock=0):
+        """
+        Our constructor class that instantiates bike rental shop.
+        """
+        self.stock = stock
+
+    def display_stock(self):
+        """
+        Displays the bikes currently available for rent in the shop.
+        """
+        print(f"We have currently {self.stock} bikes available to rent.")
+        return self.stock
+
+    def rent_bike_hourly_basis(self, n):
+        """
+        Rents a bike on hourly basis to a customer.
+        """
+        if n <= 0:
+            print("Number of bikes should be positive!")
+            return None
+        
+        elif n > self.stock:
+            print(f"Sorry! We have currently {self.stock} bikes available to rent.")
+            return None
+
+        else:
+            now = datetime.datetime.now()
+            print(f"You have rented a {n} bike(s) on hourly basis today at {now.hour} hours.")
+            print("You will be charged $5 for each hour per bike.")
+            print("We hope that you enjoy our service.")
+
+    def rent_bike_on_daily_basis(self, n):
+        """
+        Rents a bike on daily basis to a customer.
+        """
+        if n <= 0:
+            print("Number of bikes should be positive!")
+            return None
+        elif n > self.stock:
+            print(f"Sorry! We have currently {self.stock} bikes available to rent.")
+            return None
+        else:
+            now = datetime.datetime.now()
+            print("You have rented {n} bike(s) on daily basis today at {now.hour} hours.")
+            print("You will be charged $20 for each day per bike.")
+            print("We hope that you enjoy our service.")
+            self.stock -= n
+            return now
+
+    def rent_bike_on_weekly_basis(self, n):
+        """
+        Rents a bike on weekly basis to a customer.
+        """
+        if n <= 0:
+            print("Number of bikes should be positive!")
+            return None
+        elif n > self.stock:
+            print(f"Sorry! We have currently {self.stock} bike available to rent.")
+            return None
+        else:
+            now = datetime.datetime.now()
+            print(f"You have rented {n} bike(s) on weekly basis today at {now.hour} hours.")
+            print("You will be charged $60 for each week per bike.")
+            print("We hope that you enjoy our service.")
+            self.stock -= n
+            return now
+
+    def return_bike(self, request):
+        """
+        1. Accept a rented Bike from customer
+        2. Replensihes the inventory
+        3. Return a bill
+        """
+        rentalTime, rentalBasis, numOfBikes = request
+        bill = 0
+
+        if rentalTime and rentalBasis and numOfBikes:
+            self.stock += nomOfBikes
+            now = datetime.datetime.now()
+            rentalPeriod = now - rentalTime
+
+            # hourly bill calculation
+            if rentalBasis == 1:
+                bill = round(rentalPeriod.seconds / 3600) * 5 * numOfBikes
+
+            elif rentalBasis == 2:
+                bill = round(rentalPeriod.days) * 20 * numOfBikes
+
+            elif rentalBasis == 3:
+                bill = round(rentalPeriod.days / 7) * 60 * numOfBikes
+
+            if (3 <= numOfBikes <= 5):
+                print("You are eligible for family rental promotion of 30% discount")
+                bill = bill * 0.7
+
+            print("Thanks for returning your bike. Hope you enjoyed our service!")
+            print(f"That would be ${bill}")
+            return bill
+        else:
+            print("Are you sure you rented a bike with us?")
+            return None
+
+class Customer:
+    def __init__(self):
+        """
+        Our constructor method which instantiates various customer objects.
+        """
+        self.bikes = 0
+        self.rentalBasis = 0
+        self.rentalTime = 0
+        self.bill = 0
+
+    def renquest_bike(self):
+        """
+        Takes a request from the customer for the number of bikes.
+        """
+
+        bikes = input("How many bikes would you like to rent?")
+        try:
+            bikes = int(bikes)
+        except ValueError:
+            print("That's not a positive integer!")
+            return -1
+
+        if bikes < 1:
+            print("Invalid input. Number of bikes should be greater than zero!")
+            return -1
+        else:
+            self.bikes = bikes
+        return self.bikes
+
+    def return_bike(self):
+        """
+        Allows customers to return their bikes to the rental shop.
+        """
+        if self.rentalBasis and self.rentalTime and self.bikes:
+            return self.rentalTime , self.rentalBasis, self.bikes
+        else:
+            return 0, 0, 0
---
+++ main.py
@@ -0,0 +1,36 @@
+from bikeRental import BikeRental, Customer
+
+def main():
+    shop = BikeRental(100)
+    customer = Customer()
+
+    while True:
+        print("""
+        ===== Bike Rental Shop =====
+        1. Display available bikes
+        2. Request a bike on hourly basis $5
+        3. Request a bike on daily basis $20
+        4. Request a bike on weekly basis $60
+        5. Return a bike
+        6. Exit
+        """)
+
+        choice = input("Enter choice: ")
+
+        try:
+            choice = int(choice)
+        except ValueError:
+            print("That's not an int!")
+            continue
+
+        if choice == 1:
+            shop.displaystock()
+
+        elif choice == 2:
+            customer.rentalTime = shop.rentBikeOnHourlyBasis(customer.requestBike())
+            customer.rentalBasis = 1
+
+        elif choice == 3:
+
+
+main()
